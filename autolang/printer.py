bff7d8f68bb971845975d2d662a5fd46907b2607
Alvaro <alvaro@chainalysis.com>
2023-04-10 17:02:29 +0100

    Pretty printer

---
+++ autolang/printer.py
@@ -0,0 +1,28 @@
+def print_objective(objective):
+    color_print("*****Objective*****", 4)
+    print(objective)
+
+def print_task_list(complete_list, pending_list):
+    color_print("*****TASK LIST*****", 5)
+    print("Completed: ") 
+    for task in complete_list:
+        print(str(task["task_id"]) + ": " + task["task_name"])
+
+    print("\nPending: ") 
+    for task in pending_list:
+        print(str(task["task_id"]) + ": " + task["task_name"])
+
+def print_next_task(task):
+    color_print("*****NEXT TASK*****", 2)
+    print(str(task["task_id"]) + ": " + task["task_name"])
+
+def print_task_result(result):
+    color_print("*****TASK RESULT*****", 3)
+    print(result)
+
+def print_end():
+    color_print("*****TASK ENDING*****", 1)
+
+# leave at the end as the codes somehow screw up indenting in the rest of the file
+def color_print(text: str, color: int):
+    print(f"\n\033[9{color}m\033[1m{text}\033[0m\033[0m\n")
